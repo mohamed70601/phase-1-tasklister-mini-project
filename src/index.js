3bf90281473f290b62396bd5368c0f4165d05b76
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-09-11 16:38:43 +0200

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  form = document.querySelector('form')
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    buildMyTodos(e.target.new_task_description.value);
+    form.reset()
+  })
 });
+
+function buildMyTodos(todo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete);
+  btn.textContent = 'X';
+  p.textContent = `${todo} `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove();
+}
